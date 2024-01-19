4fc7ea2b2778ca529afc1d3e61ed9fac66410c0a
unknown <md.e.h.ashiq@gmail.com>
2024-01-19 20:35:04 +0600

    dashboard

--- employee/forms.py
+++ employee/forms.py
@@ -7,7 +7,16 @@ class EmployeeProfileForm(forms.ModelForm):
         model = EmployeeProfile
         fields = "__all__"
         exclude = [ 'account', 'is_set']
+
         
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control border-1 bg-light px-4',
+                'style':"height: 55px;",
+                'placeholder': self.fields[field].label if self.fields[field].label else '',
+                })     
 
 class CompanyForm(forms.ModelForm):
     
@@ -15,4 +24,11 @@ class CompanyForm(forms.ModelForm):
         model = Company
         fields = "__all__"
         
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control border-1 bg-light px-4',
+                'style':"height: 55px;",
+                'placeholder': self.fields[field].label if self.fields[field].label else '',
+                })
